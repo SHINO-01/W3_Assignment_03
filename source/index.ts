e80b1892d458d7e7cb4d4b9b41a1d4d56f265f6c
SHINO-01 <sakifhussain33@gmail.com>
2024-11-13 16:17:53 +0600

    tiny mistake

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
+import { __dirname } from './dirnameHelper';
 import { createHotel, getHotel, updateHotel, uploadImages } from './controllers/hotelController';
 
 const app = express();
